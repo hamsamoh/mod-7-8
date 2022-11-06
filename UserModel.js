2bdc86f77e4abf5132e5eca6ec10480de5e042be
hamsamoh <111298812+hamsamoh@users.noreply.github.com>
2022-11-06 21:55:19 +0300

    Add files via upload

---
+++ UserModel.js
@@ -0,0 +1,31 @@
+var mongoose = require('mongoose');
+var UserSchema = new mongoose.Schema(
+    {
+        "firstname" : {
+            type: String,
+            required: true
+        },
+        "lastname": {
+            type: String,
+            required: true
+        },
+        "email" : {
+            type: String,
+            required: true
+        },
+        "password": {
+            type: String,
+            required: true
+        },
+        "date-created": {
+            type: Date,
+            required: true,
+            default: Date.now
+        }
+    }    
+);
+
+var UserModel = mongoose.model('users', UserSchema);
+
+module.exports = UserModel;
+
